8b8a1a422886e2f1c1b24428c3e4f6136abd34f2
sygonsky00 <sygonsky@naver.com>
2020-08-04 18:42:56 +0900

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var app = express();
 
 //DB setting
@@ -9,11 +11,9 @@ mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
-
 db.once('open', function () {
   console.log('DB connected');
 });
-
 db.on('error', function (err) {
   console.log('DB ERROR : ', err);
 });
@@ -21,6 +21,13 @@ db.on('error', function (err) {
 //Other settings
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+
+//Routes
+app.use('/', require('./routes/home'));
+app.use('/contacts', require('./routes/contacts'));
 
 //Port settings
 var port = 3000;
